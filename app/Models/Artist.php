68251557e536b7924cb63ba7cc83b35cb9d6a2cf
mehdichouati <mehdicb7@gmail.com>
2026-01-21 01:27:11 +0100

    Création du modèle et migration

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -10,18 +10,37 @@ class Artist extends Model
 {
     use HasFactory;
 
+    /**
+     * Champs autorisés (mass assignment)
+     */
     protected $fillable = [
         'firstname',
         'lastname',
     ];
 
+    /**
+     * Champs interdits (vide car on utilise $fillable)
+     */
+    protected $guarded = [];
+
+    /**
+     * Champs cachés lors de la sérialisation JSON
+     */
+    protected $hidden = [];
+
+    /**
+     * Table associée
+     */
     protected $table = 'artists';
 
+    /**
+     * Pas de created_at / updated_at dans la table
+     */
     public $timestamps = false;
 
     /**
-     * An artist can have many types.
-     * Pivot table: artist_type (artist_id, type_id)
+     * Un artist peut avoir plusieurs types.
+     * Pivot: artist_type (artist_id, type_id)
      */
     public function types(): BelongsToMany
     {
